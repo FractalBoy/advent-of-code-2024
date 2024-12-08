cf93f7076d0d66878904593cff838ddbe45e5a08
Marc Reisner <reisner.marc@gmail.com>
2024-12-08 03:46:01 +0000

    Improve day 6 performance

--- solutions/day6.go
+++ solutions/day6.go
@@ -52,12 +52,10 @@ func parseDay6Input(input string) (Guard, Map) {
 	return guard, myMap
 }
 
-func (d Day6) Part1(input string) (string, error) {
-	guard, myMap := parseDay6Input(input)
-
+func getVisitedLocations(guard Guard, map_ Map) []Point {
 	visited := []Point{}
 
-	for guard.Location.X >= 0 && guard.Location.X < myMap.Size && guard.Location.Y >= 0 && guard.Location.Y < myMap.Size {
+	for guard.Location.X >= 0 && guard.Location.X < map_.Size && guard.Location.Y >= 0 && guard.Location.Y < map_.Size {
 		if !slices.Contains(visited, guard.Location) {
 			visited = append(visited, guard.Location)
 		}
@@ -73,7 +71,7 @@ func (d Day6) Part1(input string) (string, error) {
 			guard.Location.X--
 		}
 
-		if slices.Index(myMap.Obstacles, guard.Location) != -1 {
+		if slices.Index(map_.Obstacles, guard.Location) != -1 {
 			switch guard.Orientation {
 			case NORTH:
 				guard.Orientation = EAST
@@ -91,57 +89,81 @@ func (d Day6) Part1(input string) (string, error) {
 		}
 	}
 
+	return visited
+}
+
+func (d Day6) Part1(input string) (string, error) {
+	guard, map_ := parseDay6Input(input)
+
+	visited := getVisitedLocations(guard, map_)
+
 	return strconv.Itoa(len(visited)), nil
 }
 
 func (d Day6) Part2(input string) (string, error) {
-	guard, myMap := parseDay6Input(input)
+	guard, map_ := parseDay6Input(input)
 
+	visited := getVisitedLocations(guard, map_)
 	cycles := 0
 
-	for y := 0; y < myMap.Size; y++ {
-		for x := 0; x < myMap.Size; x++ {
-			clonedGuard := Guard{Location: Point{X: guard.Location.X, Y: guard.Location.Y}, Orientation: guard.Orientation}
+	for _, point := range visited {
+		clonedGuard := Guard{Location: Point{X: guard.Location.X, Y: guard.Location.Y}, Orientation: guard.Orientation}
 
-			obstacles := slices.Clone(myMap.Obstacles)
-			obstacles = append(obstacles, Point{X: x, Y: y})
+		obstacles := slices.Clone(map_.Obstacles)
+		obstacles = append(obstacles, point)
 
-			visited := []Guard{}
+		guards := make(map[int]map[int]map[int]bool)
 
-			for clonedGuard.Location.X >= 0 && clonedGuard.Location.X < myMap.Size && clonedGuard.Location.Y >= 0 && clonedGuard.Location.Y < myMap.Size {
+		for clonedGuard.Location.X >= 0 && clonedGuard.Location.X < map_.Size && clonedGuard.Location.Y >= 0 && clonedGuard.Location.Y < map_.Size {
+			switch clonedGuard.Orientation {
+			case NORTH:
+				clonedGuard.Location.Y--
+			case SOUTH:
+				clonedGuard.Location.Y++
+			case EAST:
+				clonedGuard.Location.X++
+			case WEST:
+				clonedGuard.Location.X--
+			}
+
+			if slices.Index(obstacles, clonedGuard.Location) != -1 {
 				switch clonedGuard.Orientation {
 				case NORTH:
-					clonedGuard.Location.Y--
-				case SOUTH:
+					clonedGuard.Orientation = EAST
 					clonedGuard.Location.Y++
+				case SOUTH:
+					clonedGuard.Orientation = WEST
+					clonedGuard.Location.Y--
 				case EAST:
-					clonedGuard.Location.X++
-				case WEST:
+					clonedGuard.Orientation = SOUTH
 					clonedGuard.Location.X--
+				case WEST:
+					clonedGuard.Orientation = NORTH
+					clonedGuard.Location.X++
 				}
+			} else {
+				map1, ok := guards[clonedGuard.Location.X]
 
-				if slices.Index(obstacles, clonedGuard.Location) != -1 {
-					switch clonedGuard.Orientation {
-					case NORTH:
-						clonedGuard.Orientation = EAST
-						clonedGuard.Location.Y++
-					case SOUTH:
-						clonedGuard.Orientation = WEST
-						clonedGuard.Location.Y--
-					case EAST:
-						clonedGuard.Orientation = SOUTH
-						clonedGuard.Location.X--
-					case WEST:
-						clonedGuard.Orientation = NORTH
-						clonedGuard.Location.X++
+				if ok {
+					map2, ok := map1[clonedGuard.Location.Y]
+
+					if ok {
+						exists, ok := map2[clonedGuard.Orientation]
+
+						if ok && exists {
+							cycles++
+							break
+						}
+
+						guards[clonedGuard.Location.X][clonedGuard.Location.Y][clonedGuard.Orientation] = true
+					} else {
+						guards[clonedGuard.Location.X][clonedGuard.Location.Y] = make(map[int]bool)
+						guards[clonedGuard.Location.X][clonedGuard.Location.Y][clonedGuard.Orientation] = true
 					}
 				} else {
-					if slices.Contains(visited, clonedGuard) {
-						cycles++
-						break
-					}
-
-					visited = append(visited, Guard{Location: Point{X: clonedGuard.Location.X, Y: clonedGuard.Location.Y}, Orientation: clonedGuard.Orientation})
+					guards[clonedGuard.Location.X] = make(map[int]map[int]bool)
+					guards[clonedGuard.Location.X][clonedGuard.Location.Y] = make(map[int]bool)
+					guards[clonedGuard.Location.X][clonedGuard.Location.Y][clonedGuard.Orientation] = true
 				}
 			}
 		}
